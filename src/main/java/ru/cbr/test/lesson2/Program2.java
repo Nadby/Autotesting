714e9b8dec09429bfeafbd0a39e6bfef05919a33
nadby <nadby@mail.ru>
2024-12-12 23:07:20 +0300

    удаление лишних классов из задания 2

--- src/main/java/ru/cbr/test/lesson2/Program2.java
+++
@@ -1,7 +0,0 @@
-package ru.cbr.test.lesson2;
-
-public class Program2 {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/main/java/ru/cbr/test/lesson2/Program3.java
+++
@@ -1,7 +0,0 @@
-package ru.cbr.test.lesson2;
-
-public class Program3 {
-    public static void main(String[] args) {
-        
-    }
-}
